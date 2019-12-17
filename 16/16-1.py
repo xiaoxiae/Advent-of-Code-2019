24fc706a457fee0a22d8475a1d429e43bb6b3336
xiaoxiae <tomas.slama.131@gmail.com>
2019-12-17 09:34:34 +0100

    add solution to problem 16
    The second solution takes way longer than it should, since it should be doing about 100 000 000 operations at most. This, for some reason, takes a few hours in Python. Either the algorithms is slow or Python is, but I suspect the latter :P.

---
+++ 16/16-1.py
@@ -0,0 +1,47 @@
+from typing import *
+
+
+def get_prefix_sum(s: int, e: int, prefixes: List[int]):
+    """Calculate the sum of a segment [s, e]."""
+    # restrict start and end
+    s = max(0, s)
+    e = min(len(prefixes) - 1, e)
+
+    return prefixes[e] - (0 if s - 1 < 0 else prefixes[s - 1])
+
+
+input_list = list(map(int, str(open("16.in", "r").read().strip())))
+pattern = [0, 1, 0, -1]
+delta = 1
+
+prefixes = [0] * len(input_list)
+output_list = [0] * len(input_list)
+
+for i in range(100):
+    # calculate prefix sums and zero the output list
+    total = 0
+    for i in range(len(prefixes)):
+        total += input_list[i]
+        prefixes[i] = total
+        output_list[i] = 0
+
+    for i in range(len(input_list)):
+        index = delta // (i + 1)  # index in pattern
+        j = -delta  # starting position in the list
+
+        while j < len(input_list):
+            s, e = j, j + i
+
+            # ignore nonsensical values
+            if s >= 0 or e >= 0:
+                prefix_sum = get_prefix_sum(s, e, prefixes)
+                output_list[i] += prefix_sum * pattern[index % len(pattern)]
+                index += 1
+
+            j += i + 1
+
+        output_list[i] = abs(output_list[i]) % 10
+
+    input_list, output_list = output_list, input_list
+
+print("".join(list(map(str, input_list[:8]))))
